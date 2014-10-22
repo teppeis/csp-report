d94b48f9bc91b787c49641220d8653cf7350f708
teppeis <teppeis@gmail.com>
2014-10-22 21:34:07 +0900

    Fix json reporter

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,21 +1,18 @@
 var socket = io('http://india.dev.cybozu.co.jp/');
 socket.on('report', function (data) {
     console.log(data);
-    var str = JSON.stringify(data);
+    var str = JSON.stringify(data['csp-report']);
     var container = document.getElementById('container');
     var div = document.createElement('div');
-    // TODO: escape
     div.innerHTML = htmlspecialchars(str);
     container.appendChild(div);
 });
 
 function htmlspecialchars(str) {
-    if (typeof(str) == "string") {
-        str = str.replace(/&/g, "&amp;"); /* must do &amp; first */
-        str = str.replace(/"/g, "&quot;");
-        str = str.replace(/'/g, "&#039;");
-        str = str.replace(/</g, "&lt;");
-        str = str.replace(/>/g, "&gt;");
-    }
+    str = str.replace(/&/g, "&amp;");
+    str = str.replace(/"/g, "&quot;");
+    str = str.replace(/'/g, "&#039;");
+    str = str.replace(/</g, "&lt;");
+    str = str.replace(/>/g, "&gt;");
     return str;
 }
