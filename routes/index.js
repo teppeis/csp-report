1c12a40b4941877720c8c15651f10586f5c20360
teppeis <teppeis@gmail.com>
2014-10-22 20:59:55 +0900

    add unsafeInline param

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'CSP Report' });
 });
 
+router.get('/csp-demo', function(req, res) {
+  var nonce = new Date().valueOf();
+  var cspHeaderName = 'Content-Security-Policy';
+  console.log(req.query);
+  if (req.query.unsafeInline) {
+    cspHeaderValue = "script-src 'self' 'nonce-" + nonce + "' 'unsafe-inline' http://ajax.googleapis.com; report-uri /report";
+  } else {
+    cspHeaderValue = "script-src 'self' 'nonce-" + nonce + "' http://ajax.googleapis.com; report-uri /report";
+  }
+  res.header(cspHeaderName, cspHeaderValue);
+  var cspHeader = cspHeaderName + ': ' + cspHeaderValue;
+  res.render('csp-demo', {title: 'CSP Report', nonce: nonce, cspHeader: cspHeader});
+});
+
 module.exports = router;
